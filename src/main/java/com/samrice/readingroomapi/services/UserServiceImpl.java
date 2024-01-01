74a3b8782170b119c04f40ab4326a3b4593245bb
Sam Rice <samsrice@gmail.com>
2024-01-01 13:17:02 -0800

    set up basic User db services

--- src/main/java/com/samrice/readingroomapi/services/UserServiceImpl.java
+++ src/main/java/com/samrice/readingroomapi/services/UserServiceImpl.java
@@ -2,9 +2,19 @@ package com.samrice.readingroomapi.services;
 
 import com.samrice.readingroomapi.domain.User;
 import com.samrice.readingroomapi.exceptions.RrAuthException;
+import com.samrice.readingroomapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.regex.Pattern;
+
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public User validateUser(String email, String password) throws RrAuthException {
@@ -13,6 +23,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(String firstName, String lastName, String email, String password) throws RrAuthException {
-        return null;
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if (email != null) email = email.toLowerCase();
+
+        if (!pattern.matcher(email).matches()) {
+            throw new RrAuthException("Invalid email format");
+        }
+
+        Integer count = userRepository.getCountByEmail(email);
+        if (count > 0) {
+            throw new RrAuthException("Email already in use");
+        }
+
+        Integer userId = userRepository.create(firstName, lastName, email, password);
+        return userRepository.findById(userId);
     }
 }
